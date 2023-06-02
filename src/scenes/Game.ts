b3ef3d042fbf72de793d0b8d4ccdc9ac7a60a731
TPofahl <thomas.pofahl@blendinteractive.com>
2023-06-02 15:28:45 -0500

    change background position

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -5,6 +5,7 @@ import Player from '../Entities/Player.js';
 import Camera, { camera } from '../Entities/Camera.js';
 import { Rect } from 'kaboom';
 import { level1, levelOptions } from './Level-001.js';
+import { playerEntity } from "../Entities/Player.js";
 
 export default function Game() {
     const {
@@ -19,7 +20,7 @@ export default function Game() {
     const world = level1;
     addLevel(world, levelOptions);
 
-    layer(["bg", "bg2", "world", "ui"], "obj");
+    layer(["bg2", "bg", "world", "ui"], "obj");
 
     var bgArrayLayer1 = [];
     var bgArrayLayer2 = [];
@@ -29,33 +30,33 @@ export default function Game() {
         layer("bg"),
         "bg",
         pos(0, 0),
-        scale(2, 2),
-        z(-10)
+        scale(1, 1),
+        z(-10),
     ]));
     bgArrayLayer2.push(add([
         sprite(ASSETS.BACKGROUND2),
         layer("bg2"),
         "bg2",
         pos(0, 0),
-        scale(2, 2),
-        z(-20)
+        scale(1, 1),
+        z(-20),
     ]));
 
-    for (let i = 1; i < 30; i++) {
+    for (let i = 1; i < 3; i++) {
         var bg = add([
             sprite(ASSETS.BACKGROUND),
             layer("bg"),
             "bg",
-            pos(bgArrayLayer1[(i - 1)].pos.x + 32, 0),
-            scale(2, 2),
+            pos(bgArrayLayer1[(i - 1)].pos.x + 600, 0),
+            scale(1, 1),
             z(-10)
         ]);
         var bg2 = add([
             sprite(ASSETS.BACKGROUND2),
             layer("bg2"),
             "bg2",
-            pos(bgArrayLayer2[(i - 1)].pos.x + 32, 0),
-            scale(2, 2),
+            pos(bgArrayLayer2[(i - 1)].pos.x + 600, 0),
+            scale(1, 1),
             z(-20)
         ]);
 
@@ -64,16 +65,18 @@ export default function Game() {
     };
 
     onUpdate(() => {
+        console.log("camera pos: ", camera.pos.y);
         bgArrayLayer1.forEach(element => {
             element.pos.x -= 1;
+            element.pos.y = playerEntity.pos.y - 150;
         });
-        if (bgArrayLayer1[0].pos.x <= camera.pos.x - 500) {
+        if (bgArrayLayer1[0].pos.x <= camera.pos.x - 1500) {
             destroy(bgArrayLayer1.shift());
             var bg = add([
                 sprite(ASSETS.BACKGROUND),
                 layer("bg"),
                 "bg",
-                pos(bgArrayLayer1[(bgArrayLayer1.length - 1)].pos.x + 32, 0),
+                pos(bgArrayLayer1[(bgArrayLayer1.length - 1)].pos.x + 600, 0),
                 scale(2, 2),
                 z(-10)
             ]);
@@ -82,14 +85,15 @@ export default function Game() {
 
         bgArrayLayer2.forEach(element => {
             element.pos.x -= 2;
+            element.pos.y = playerEntity.pos.y - 150;
         });
-        if (bgArrayLayer2[0].pos.x <= camera.pos.x - 500) {
+        if (bgArrayLayer2[0].pos.x <= camera.pos.x - 1500) {
             destroy(bgArrayLayer2.shift());
             var bg2 = add([
                 sprite(ASSETS.BACKGROUND2),
                 layer("bg2"),
                 "bg2",
-                pos(bgArrayLayer2[(bgArrayLayer2.length - 1)].pos.x + 32, 0),
+                pos(bgArrayLayer2[(bgArrayLayer2.length - 1)].pos.x + 600, 0),
                 scale(2, 2),
                 z(-20)
             ]);
