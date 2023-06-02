f8ee38a7bf6045071669c4110b969bc567fcd796
TPofahl <thomas.pofahl@blendinteractive.com>
2023-06-02 13:44:27 -0500

    modifying parallax

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,7 +2,7 @@ import k from '../kaboom';
 import assets, { ASSETS, SOUNDS } from '../assets';
 import Helicopter from '../Entities/Helicopter.js';
 import Player from '../Entities/Player.js';
-import Camera from '../Entities/Camera.js';
+import Camera, { camera } from '../Entities/Camera.js';
 import { Rect } from 'kaboom';
 
 export default function Game() {
@@ -41,6 +41,7 @@ export default function Game() {
       
       var bgArrayLayer1 = [];
       var bgArrayLayer2 = [];
+
       bgArrayLayer1.push(add([
         sprite(ASSETS.BACKGROUND),
         layer("bg"),
@@ -56,12 +57,13 @@ export default function Game() {
         scale(2,2)
       ]));
 
-      for (let i = 1; i < 300; i++) {
+      for (let i = 1; i < 30; i++) {
           var bg = add([
             sprite(ASSETS.BACKGROUND),
             layer("bg"),
             "bg",
             pos(bgArrayLayer1[(i - 1)].pos.x + 32, 0),
+            move(-1,0),
             scale(2,2),
           ]);
           var bg2 = add([
@@ -69,18 +71,47 @@ export default function Game() {
             layer("bg2"),
             "bg2",
             pos(bgArrayLayer2[(i - 1)].pos.x + 32, 0),
+            move(-2,0),
             scale(2,2),
           ]);
 
           bgArrayLayer1.push(bg);
           bgArrayLayer2.push(bg2);
       };
-    addLevel(world[0], leveloptions);
+      addLevel(world[0], leveloptions);
 
+      bg.onUpdate(() => {   
+        if (bgArrayLayer1[0].pos.x <= camera.pos.x - 500) {
+            destroy(bgArrayLayer1.shift());
+            var newBgTile = add([
+                sprite(ASSETS.BACKGROUND),
+                layer("bg"),
+                "bg",
+                pos(bgArrayLayer1[(bgArrayLayer1.length - 1)].pos.x + 32, 0),
+                move(-1,0),
+                scale(2,2)
+            ]);
+            bgArrayLayer1.push(newBgTile);
+            console.log("total size layer 1: ", bgArrayLayer1.length)
+        }
+      });
       bg2.onUpdate(() => {   
-        bgArrayLayer2.forEach(element => {
-            element.pos.x -= 2;
-        });
+        // bgArrayLayer2.forEach(element => {
+        //     element.pos.x -= 2;
+        // });
+        if (bgArrayLayer2[0].pos.x <= camera.pos.x - 500) {
+            destroy(bgArrayLayer2.shift());
+            var newBgTile = add([
+                sprite(ASSETS.BACKGROUND2),
+                layer("bg2"),
+                "bg2",
+                pos(bgArrayLayer2[(bgArrayLayer2.length - 1)].pos.x + 32, 0),
+                move(-2,0),
+                scale(2,2)
+            ]);
+            bgArrayLayer2.push(newBgTile);
+            console.log("total size layer 2: ", bgArrayLayer2.length)
+        }
       });
     Player();
     Camera();
